a7a945623d5a72eac1a4b32af1af43fd11c5c1a4
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-04-26 22:33:00 +0900

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,21 @@ use rand::{
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
+const FIELD_WIDTH: usize = 13;
+const FIELD_HEIGHT: usize = 21;
+type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+
+#[derive(Clone, Copy)]
+enum BlockKind {
+    I,
+    O,
+    S,
+    Z,
+    J,
+    L,
+    T,
+}
+
 impl Distribution<BlockKind> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> BlockKind {
         match rng.gen_range(0..=6) {
@@ -20,37 +35,29 @@ impl Distribution<BlockKind> for Standard {
     }
 }
 
-#[derive(Clone, Copy)]
-enum BlockKind {
-    I,
-    O,
-    S,
-    Z,
-    J,
-    L,
-    T,
-}
-
 type BlockShape = [[usize; 4]; 4];
+
+const BLOCKS: [BlockShape; 7] = [
+    // I
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+    // O
+    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]],
+    // S
+    [[0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0]],
+    // Z
+    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [0, 0, 0, 0]],
+    // J
+    [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    // L
+    [[0, 0, 0, 0], [0, 0, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    // T
+    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+];
 
 struct Pos {
     x: usize,
     y: usize,
 }
-
-const BLOCKS: [BlockShape; 7] = [
-    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]], // I
-    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]], // O
-    [[0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0]], // S
-    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [0, 0, 0, 0]], // Z
-    [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]], // J
-    [[0, 0, 0, 0], [0, 0, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]], // L
-    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]], // T
-];
-
-const FIELD_WIDTH: usize = 13;
-const FIELD_HEIGHT: usize = 21;
-type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
 
 fn is_collision(field: &Field, pos: &Pos, block: BlockKind) -> bool {
     for y in 0..4 {
