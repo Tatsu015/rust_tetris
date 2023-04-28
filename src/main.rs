a4a7254d72ff10b52f95e747936472a021915694
Tatsu015 <tatsumi.soichiro@gmail.com>
2023-04-28 22:59:04 +0900

    add game struct

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,13 @@ struct Pos {
     y: usize,
 }
 
-fn is_collision(field: &Field, pos: &Pos, block: BlockKind) -> bool {
+fn is_collision(Game { field, pos, block }: &Game) -> bool {
     for y in 0..4 {
         for x in 0..4 {
             if x + pos.x >= FIELD_WIDTH {
                 continue;
             }
-            if field[y + pos.y][x + pos.x] & BLOCKS[block as usize][y][x] == 1 {
+            if field[y + pos.y][x + pos.x] & BLOCKS[*block as usize][y][x] == 1 {
                 return true;
             }
         }
@@ -29,11 +29,11 @@ fn is_collision(field: &Field, pos: &Pos, block: BlockKind) -> bool {
     return false;
 }
 
-fn draw(field: &Field, pos: &Pos, block: BlockKind) {
+fn draw(Game { field, pos, block }: &Game) {
     let mut field_with_block = field.clone();
     for y in 0..4 {
         for x in 0..4 {
-            if BLOCKS[block as usize][y][x] == 1 {
+            if BLOCKS[*block as usize][y][x] == 1 {
                 field_with_block[y + pos.y][x + pos.x] = 1
             }
         }
@@ -52,84 +52,85 @@ fn draw(field: &Field, pos: &Pos, block: BlockKind) {
     }
 }
 
-fn main() {
-    let field = Arc::new(Mutex::new([
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ]));
+struct Game {
+    field: Field,
+    pos: Pos,
+    block: BlockKind,
+}
 
-    let pos = Arc::new(Mutex::new(Pos { x: 4, y: 0 }));
-    let block = Arc::new(Mutex::new(rand::random::<BlockKind>()));
+fn main() {
+    let game = Arc::new(Mutex::new(Game {
+        field: [
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ],
+        pos: Pos { x: 4, y: 0 },
+        block: rand::random::<BlockKind>(),
+    }));
 
     println!("\x1b[2J\x1b[H\x1b[?25l");
 
-    draw(
-        &field.lock().unwrap(),
-        &pos.lock().unwrap(),
-        *block.lock().unwrap(),
-    );
+    draw(&game.lock().unwrap());
 
     {
-        let pos = Arc::clone(&pos);
-        let field = Arc::clone(&field);
-        let block = Arc::clone(&block);
+        let game = Arc::clone(&game);
         let _ = thread::spawn(move || loop {
             thread::sleep(time::Duration::from_millis(500));
 
-            let mut pos = pos.lock().unwrap();
-            let mut field = field.lock().unwrap();
-            let mut block = block.lock().unwrap();
+            let mut game = game.lock().unwrap();
             let new_pos = Pos {
-                x: pos.x,
-                y: pos.y + 1,
+                x: game.pos.x,
+                y: game.pos.y + 1,
             };
-            if !is_collision(&field, &new_pos, *block) {
-                *pos = new_pos
+            if !is_collision(&game) {
+                game.pos = new_pos
             } else {
+                let gx = game.pos.x;
+                let gy = game.pos.y;
                 for y in 0..4 {
                     for x in 0..4 {
-                        if BLOCKS[*block as usize][y][x] == 1 {
-                            field[y + pos.y][x + pos.x] = 1;
+                        if BLOCKS[game.block as usize][y][x] == 1 {
+                            game.field[y + gy][x + gx] = 1;
                         }
                     }
                 }
                 for y in 1..FIELD_HEIGHT - 1 {
                     let mut can_elase = true;
                     for x in 1..FIELD_WIDTH - 1 {
-                        if field[y][x] == 0 {
+                        if game.field[y][x] == 0 {
                             can_elase = false;
                             break;
                         }
                     }
                     if can_elase {
                         for ty in (2..=y).rev() {
-                            field[ty] = field[ty - 1]
+                            game.field[ty] = game.field[ty - 1]
                         }
                     }
                 }
-                *pos = Pos { x: 4, y: 0 };
-                *block = rand::random();
+                game.pos = Pos { x: 4, y: 0 };
+                game.block = rand::random();
             }
-            draw(&field, &pos, *block);
+            draw(&game);
         });
     }
 
@@ -137,43 +138,38 @@ fn main() {
     loop {
         match g.getch() {
             Ok(Key::Left) => {
-                let mut pos = pos.lock().unwrap();
-                let field = field.lock().unwrap();
-                let block = block.lock().unwrap();
+                let mut game = game.lock().unwrap();
+
                 let new_pos = Pos {
-                    x: pos.x.checked_sub(1).unwrap_or_else(|| pos.x),
-                    y: pos.y,
+                    x: game.pos.x.checked_sub(1).unwrap_or_else(|| game.pos.x),
+                    y: game.pos.y,
                 };
-                if !is_collision(&field, &new_pos, *block) {
-                    *pos = new_pos
+                if !is_collision(&game) {
+                    game.pos = new_pos
                 }
-                draw(&field, &pos, *block);
+                draw(&game);
             }
             Ok(Key::Down) => {
-                let mut pos = pos.lock().unwrap();
-                let field = field.lock().unwrap();
-                let block = block.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Pos {
-                    x: pos.x,
-                    y: pos.y + 1,
+                    x: game.pos.x,
+                    y: game.pos.y + 1,
                 };
-                if !is_collision(&field, &new_pos, *block) {
-                    *pos = new_pos
+                if !is_collision(&game) {
+                    game.pos = new_pos
                 }
-                draw(&field, &pos, *block);
+                draw(&game);
             }
             Ok(Key::Right) => {
-                let mut pos = pos.lock().unwrap();
-                let field = field.lock().unwrap();
-                let block = block.lock().unwrap();
+                let mut game = game.lock().unwrap();
                 let new_pos = Pos {
-                    x: pos.x + 1,
-                    y: pos.y,
+                    x: game.pos.x + 1,
+                    y: game.pos.y,
                 };
-                if !is_collision(&field, &new_pos, *block) {
-                    *pos = new_pos
+                if !is_collision(&game) {
+                    game.pos = new_pos
                 }
-                draw(&field, &pos, *block);
+                draw(&game);
             }
             Ok(Key::Char('q')) => break,
             _ => (),
